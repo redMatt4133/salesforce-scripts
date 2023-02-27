bf90efec25225c3bb488e5f3b10194889acd8ecc
Orange-Lightning <90224411+redMatt4133@users.noreply.github.com>
2023-02-27 11:41:53 -0500

    Update deploy_metadata_sfdx.py
    Use threading to create the Classic Salesforce URL link when it's generated by the deploy function.

--- deploy_metadata_sfdx.py
+++ deploy_metadata_sfdx.py
@@ -6,6 +6,7 @@ import logging
 import re
 import subprocess
 import sys
+import threading
 
 # format logger
 logging.basicConfig(format='%(message)s', level=logging.DEBUG)
@@ -17,6 +18,10 @@ def parse_args():
         tests - required Apex tests to run against
         manifest - path to the package.xml file
         wait - number of minutes to wait for command to complete
+        environment - Salesforce environment URL
+        log - deploy log where the output of this script is being written to
+            python ./deploy_metadata_sfdx.py --args | tee -a deploy_log.txt
+            -a flag required to append to file during run-time
         validate - set to True to run validation only deployment (for quick deploys)
         debug - print command rather than run
     """
@@ -24,10 +29,35 @@ def parse_args():
     parser.add_argument('-t', '--tests')
     parser.add_argument('-m', '--manifest', default='manifest/package.xml')
     parser.add_argument('-w', '--wait', default=33)
+    parser.add_argument('-e', '--environment')
+    parser.add_argument('-l', '--log', default='deploy_log.txt')
     parser.add_argument('-v', '--validate', default=False, action='store_true')
     parser.add_argument('-d', '--debug', default=False, action='store_true')
     args = parser.parse_args()
     return args
+
+
+def create_sf_link(sf_env, log):
+    """
+        Function to check the deploy log for the ID
+        and build the URL.
+    """
+    pattern = r'Deploy ID: (.*)'
+    classic_sf_path = '/changemgmt/monitorDeploymentsDetails.apexp?retURL=' +\
+                        '/changemgmt/monitorDeployment.apexp&asyncId='
+
+    # keep reading the log until the ID has been found
+    with open(log, 'r', encoding='utf-8') as deploy_file:
+        while True:
+            file_line = deploy_file.readline()
+            match = re.search(pattern, file_line)
+            # if regex is found, build the link and break the loop
+            if match:
+                deploy_id = match.group(1)
+                sf_id = deploy_id[:-3]
+                deploy_url = sf_env + classic_sf_path + sf_id
+                logging.info(deploy_url)
+                break
 
 
 def run_command(cmd):
@@ -36,9 +66,10 @@ def run_command(cmd):
     """
     try:
         subprocess.run(cmd, check=True, shell=True)
-    except subprocess.CalledProcessError:
-        logging.warning('Deployment failed. Check the lines above for the error.')
-        sys.exit(1)
+    except subprocess.CalledProcessError as exception:
+        logging.warning('Deploy Failed. Check the lines above for the error.')
+        # store the exception in the thread object
+        threading.current_thread().exc = exception 
 
 
 def remove_spaces(string):
@@ -49,7 +80,7 @@ def remove_spaces(string):
     return re.sub(pattern, '', string)
 
 
-def main(tests, manifest, wait, validate, debug):
+def main(tests, manifest, wait, environment, log, validate, debug):
     """
         Main function to deploy metadata to Salesforce.
     """
@@ -64,14 +95,29 @@ def main(tests, manifest, wait, validate, debug):
     if validate:
         command = command + ' -c'
 
-    # Run the command
     if not debug:
         logging.info(command)
-        run_command(command)
+        # create and start threads
+        read_thread = threading.Thread(target=create_sf_link, args=(environment, log))
+        deploy_thread = threading.Thread(target=run_command, args=(command,))
+        read_thread.start()
+        deploy_thread.start()
+
+        # wait for deploy to finish
+        deploy_thread.join()
+    
+        # kill read thread if it's still running
+        # ex: if package.xml is empty, no ID is created
+        if read_thread.is_alive():
+            read_thread.terminate()
+
+        # exit with error if exception attribute is found
+        if hasattr(deploy_thread, 'exc'):
+            sys.exit(1)
     else:
         print(command)
 
 
 if __name__ == '__main__':
     inputs = parse_args()
-    main(inputs.tests, inputs.manifest, inputs.wait, inputs.validate, inputs.debug)
+    main(inputs.tests, inputs.manifest, inputs.wait, inputs.environment, inputs.log, inputs.validate, inputs.debug)
