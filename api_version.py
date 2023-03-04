3e488c6d93eaa58ea0eb6a4ecfd3b3956c185f4d
Orange-Lightning <90224411+redMatt4133@users.noreply.github.com>
2023-03-04 15:08:45 -0500

    Write the latest API version to the sfdx-project.json file

--- api_version.py
+++ api_version.py
@@ -1,10 +1,14 @@
 """
     The URL should be 'https://org.mysalesforce.com/services/data' depending on your
     Salesforce instance (org).
+    This script can be ran as a scheduled pipeline to periodically check and update 
+    the sfdx-project.json file with the latest API version.
 """
 import argparse
 import json
 import logging
+import os
+import sys
 import urllib.request
 
 # format logger
@@ -15,9 +19,11 @@ def parse_args():
     """
         Function to parse required arguments.
         url - URL which contains all supported API versions
+        file - sfdx-project.json
     """
     parser = argparse.ArgumentParser(description='A script to determine the latest API version.')
     parser.add_argument('-u', '--url')
+    parser.add_argument('-f', '--file', default='./sfdx-project.json')
     args = parser.parse_args()
     return args
 
@@ -40,17 +46,38 @@ def find_latest_version(salesforce_url):
     return max(api_versions)
 
 
-def main(url):
+def update_json_file(latest_version, json_path):
     """
-        Main function to return the latest API version.
+        Function to update the JSON file with
+        the latest API version.
     """
-    latest_version = find_latest_version(url)
+    with open(os.path.abspath(json_path), encoding='utf-8') as json_file:
+        parsed_json = json.load(json_file)
 
-    logging.info('The latest API version is: %s', latest_version)
-    # print the version to store in a variable set by the terminal
-    print(latest_version)
+    source_api_version = parsed_json.get('sourceApiVersion')
+    if source_api_version:
+        # convert the latest version (float) to a string
+        if str(latest_version) == source_api_version:
+            logging.info('The JSON file already has the latest API version.')
+            sys.exit(1)
+        else:
+            logging.info('Updating the JSON file to have the latest API version.')
+            parsed_json['sourceApiVersion'] = f"{latest_version}"
+
+    with open(os.path.abspath(json_path), 'w', encoding='utf-8') as json_file:
+        json.dump(parsed_json, json_file, indent=4, sort_keys=True)
+
+
+def main(url, json_file):
+    """
+        Main function to return the latest API version
+        and update the JSON file.
+    """
+    latest_api_version = find_latest_version(url)
+    logging.info('The latest API version is: %s', latest_api_version)
+    update_json_file(latest_api_version, json_file)
 
 
 if __name__ == '__main__':
     inputs = parse_args()
-    main(inputs.url)
+    main(inputs.url, inputs.file)
