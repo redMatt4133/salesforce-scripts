340aa6fa6c005f796711ba1a0f8caef89f8afe37
Matt Carvin <90224411+redMatt4133@users.noreply.github.com>
2023-03-12 10:05:47 -0400

    Update merge_manual_package.py

--- merge_manual_package.py
+++ merge_manual_package.py
@@ -1,32 +1,36 @@
 """
-    Import ElementTree to parse the manual packae.xml
+    Import ElementTree to parse the manual package.xml
 """
+import logging
+import re
 import xml.etree.ElementTree as ET
 
-# register_namespace only influences serialization, not search
-# use both
-METADATA_LINK = 'http://soap.sforce.com/2006/04/metadata'
-ET.register_namespace('', METADATA_LINK)
-ns = {'sforce': METADATA_LINK}
+logging.basicConfig(format='%(message)s', level=logging.DEBUG)
+# register the namespace to search the XML
+ns = {'sforce': 'http://soap.sforce.com/2006/04/metadata'}
 
 
 def parse_manual_package(package_path, changes):
     """
-        Parse the manual package.xml and
-        append the metadata types to the existing
-        changes dictionary
+        Parse the manual package.xml
+        and append the metadata types
+        to the existing changes dictionary
     """
     root = ET.parse(package_path).getroot()
 
     for metadata_type in root.findall('sforce:types', ns):
         metadata_name = (metadata_type.find('sforce:name', ns)).text
-        # find all matches if there are multiple memebers for 1 type
+        # find all matches if there are multiple members for 1 metadata type
         metadata_member_list = metadata_type.findall('sforce:members', ns)
         for metadata_member in metadata_member_list:
-            if (metadata_name is not None and len(metadata_name.strip()) > 0):
+            # if a wilcard is present in the member, don't process it
+            wildcard = re.search(r'\*', metadata_member.text)
+            if (metadata_name is not None and wildcard is None and len(metadata_name.strip()) > 0) :
                 if metadata_name in changes and changes[metadata_name] is not None:
                     changes[metadata_name].add(metadata_member.text)
                 else:
                     changes.update({metadata_name : set()})
                     changes[metadata_name].add(metadata_member.text)
+            elif wildcard:
+                logging.warning('WARNING: Wildcards are not allowed in the package.xml')
     return changes
